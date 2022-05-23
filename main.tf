5c0425860bd2ac48abeb403f659f1ff9d52bc77e
Ray Epps <rayharryepps@gmail.com>
2022-05-22 23:18:25 -0600

    move privelged mode

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ resource "aws_codebuild_project" "main" {
   build_timeout  = var.timeout
   queued_timeout = 5
   service_role   = aws_iam_role.main.arn
-  privileged_mode = true
 
   artifacts {
     type = "NO_ARTIFACTS"
@@ -45,6 +44,7 @@ resource "aws_codebuild_project" "main" {
     image                       = var.image
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
 
     dynamic "environment_variable" {
       for_each = local.project_envvars
